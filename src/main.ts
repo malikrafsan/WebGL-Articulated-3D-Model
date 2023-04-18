8de87e6cf2868a89bdd6231c49c3eb67c66697b5
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-04-18 10:15:52 +0700

    feat: add dynamic sliders

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,75 @@ import {
   Renderer,
 } from ".";
 
+const tree = (model: ArticulatedModel, level: number = 0) => {
+  const t = {
+    name: model.name,
+    ref: model,
+    level: level,
+    children: model.children.map((child) => tree(child, level + 1)),
+  } as const;
+
+  return t;
+};
+
+interface ITree {
+  name: string;
+  ref: ArticulatedModel;
+  level: number;
+  children: ITree[];
+}
+
+const mapperTree = (model: ArticulatedModel, level: number = 0) => {
+  const tree: ITree = {
+    name: model.name,
+    level: level,
+    ref: model,
+    children: model.children.map((child) => mapperTree(child, level + 1)),
+  } as const;
+
+  return tree;
+};
+
+interface ITreeSlider extends ITree {
+  slider: HTMLElement;
+}
+
+const mapTreeToSlider = (elmtContainer: ElmtContainer, tree: ITree) => {
+  const slider = elmtContainer.createSlider({
+    id: "slider-" + tree.name,
+    min: -180,
+    max: 180,
+    value: 0,
+    callbackOnChange: (value: number) => {
+      tree.ref.transform.rotation[0] = value;
+    },
+  });
+  elmtContainer.addElmt("#sliders", slider);
+
+  const sliderTree: ITreeSlider = {
+    ...tree,
+    slider: slider,
+    children: tree.children.map((child) =>
+      mapTreeToSlider(elmtContainer, child)
+    ),
+  };
+
+  return sliderTree;
+};
+
+const duplicateStr = (str: string, n: number) => {
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += str;
+  }
+  return result;
+};
+
+const printTree = (tree: any) => {
+  console.log(duplicateStr("\t", tree.level) + tree.name);
+  tree.children.forEach((child: any) => printTree(child));
+};
+
 const main = async () => {
   const vertexShaderScript = await FileManager.readFile(
     CONFIG_PATH.VERTEX_SHADER
@@ -23,6 +92,14 @@ const main = async () => {
   const articulatedModel = new ArticulatedModel(contextGL, CUBES);
   const renderer = new Renderer(contextGL);
   renderer.setModel(articulatedModel);
+
+  const t = tree(articulatedModel);
+  console.log(t);
+  printTree(t);
+
+  const t2 = mapperTree(articulatedModel);
+  mapTreeToSlider(elmtContainer, t2);
+
   // renderer.render();
 
   requestAnimationFrame(renderer.render.bind(renderer));
--- src/structs/ElmtContainer.ts
+++ src/structs/ElmtContainer.ts
@@ -1,5 +1,6 @@
 export class ElmtContainer {
   public readonly canvas: HTMLCanvasElement;
+  public readonly mapElmts: Map<string, HTMLElement> = new Map();
 
   constructor() {
     const canvas = document.querySelector("#canvas");
@@ -9,5 +10,72 @@ export class ElmtContainer {
     }
 
     this.canvas = canvas;
+  }
+
+  public store(elmt: HTMLElement, key: string) {
+    this.mapElmts.set(key, elmt);
+  }
+
+  public addElmt(parentSelector: string, elmt: HTMLElement) {
+    const parent = document.querySelector(parentSelector);
+    if (!parent) {
+      throw new Error("No parent found");
+    }
+
+    parent.appendChild(elmt);
+  }
+
+  public getElmt(key: string) {
+    const elmt = this.mapElmts.get(key);
+    if (!elmt) {
+      throw new Error("No element found");
+    }
+
+    return elmt;
+  }
+
+  public unStore(key: string) {
+    this.mapElmts.delete(key);
+  }
+
+  public removeElmt(key: string) {
+    const elmt = this.getElmt(key);
+    elmt.remove();
+    this.unStore(key);
+  }
+
+  public clear() {
+    this.mapElmts.forEach((elmt, key) => {
+      elmt.remove();
+      this.unStore(key);
+    });
+  }
+
+  public createSlider(props: {
+    id: string;
+    min: number;
+    max: number;
+    value: number;
+    callbackOnChange: (value: number) => void;
+  }) {
+    const { id, min, max, value, callbackOnChange } = props;
+    const slider = document.createElement("input");
+    slider.type = "range";
+    slider.id = id;
+    slider.min = min.toString();
+    slider.max = max.toString();
+    slider.value = value.toString();
+    slider.onchange = (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      callbackOnChange(+target.value);
+    };
+
+    const div = document.createElement("div");
+    const label = document.createElement("label");
+    label.innerText = id;
+    div.appendChild(label);
+    div.appendChild(slider);
+
+    return div;
   }
 }
